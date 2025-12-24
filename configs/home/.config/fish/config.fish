75201ff178cbbbd086080a420dda668e351ef884
Serhii Shuliak <sergshulya@gmail.com>
2025-12-24 17:38:28 +0200

    Backup 2025-12-24 17:38:28

--- configs/home/.config/fish/config.fish
+++ configs/home/.config/fish/config.fish
@@ -3,6 +3,10 @@ set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -gx SYSTEMD_EDITOR nvim
 
+alias reload='source ~/.config/fish/config.fish'
+alias reload-waybar='pkill waybar; nohup waybar >/dev/null 2>&1 & disown'
+alias glow='glow -p'
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
