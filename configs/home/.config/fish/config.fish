a403f4eae128014002c760eeebed43af1115c5f9
Serhii Shuliak <sergshulya@gmail.com>
2025-12-25 04:32:04 +0200

    Backup 2025-12-25 04:32:04

--- configs/home/.config/fish/config.fish
+++ configs/home/.config/fish/config.fish
@@ -7,7 +7,6 @@ alias reload='source ~/.config/fish/config.fish'
 alias reload-waybar='pkill waybar; nohup waybar >/dev/null 2>&1 & disown'
 alias glow='glow -p'
 alias music='ncmpcpp'
-alias pi5ssh='sshfs sshuliak@192.168.1.105:/home/sshuliak ~/mnt/pi5'
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
